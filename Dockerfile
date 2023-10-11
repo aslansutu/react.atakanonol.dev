c7382dab348c009df76c11e5826cfa2dc268ba24
Atakan Önol <atakan.onol@oruba.com.tr>
2023-10-11 12:05:07 +0300

    - Dockerize application. - Under construction message

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# Stage 1: Build the Angular App
+FROM node:latest as build
+
+WORKDIR /app
+
+# Copy package.json and package-lock.json (if available)
+COPY package*.json ./
+
+# Install app dependencies
+RUN npm install
+
+# Copy app source code
+COPY . .
+
+# Build the Angular app with production configuration
+RUN npm run build
+
+# Stage 2: Serve the Angular App with NGINX
+FROM nginx:alpine
+
+# Copy the compiled app from the build stage to NGINX's HTML directory
+COPY --from=build /app/build /usr/share/nginx/html
+
+
+# Expose port 80 (default for HTTP)
+EXPOSE 80
+
+# Start NGINX when the container runs
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This React site is under construction. <br />
+          Please navigate to the blog page for more information. <br />
+          <a href="blog.atakanonol.dev"><button class="fancy">Blog</button></a>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
